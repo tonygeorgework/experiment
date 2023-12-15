cca894dccdd46f38f9eab69fd4a1b1986992decb
tonygeorgework <149863010+tonygeorgework@users.noreply.github.com>
2023-12-15 18:24:55 +0530

    Update my_third_dbt_model.sql

--- models/my_third_dbt_model.sql
+++ models/my_third_dbt_model.sql
@@ -1,3 +1,3 @@
 {{ config(materialized='table') }}
 
-select * from {{ source('fivetran_poc_schema_mydatabase', 'CUSTOMER') }}
+select * from {{ source('fivetran_poc_schema_mydatabase', 'customer') }}
